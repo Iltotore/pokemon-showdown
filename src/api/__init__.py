31abb12b310c7491326baecc5ec7af958aaa09e5
Iltotore <rafbodaha@gmail.com>
2021-04-13 11:14:26 +0200

    fix: No-callback message weren't handled correctly
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,4 +1,10 @@
-from typing import List
+from typing import List, Callable
+
+
+class WithHeader:
+
+    def __init__(self, f):
+        self.f = f
 
 
 class Client:
@@ -8,10 +14,16 @@ class Client:
 
     def on_message_received(self, header: str, values: List[str]):
         bindings = self.receive_bindings.get(header)
+        print(bindings)
         if bindings is not None:
+            if isinstance(bindings, Callable):
+                bindings = [bindings]
             for callback in bindings:
-                callback(values)
-                self.on_registered_message_received(header, values)
+                if isinstance(callback, WithHeader):
+                    callback.f(header, values)
+                else:
+                    callback(values)
+            self.on_registered_message_received(header, values)
 
     def on_registered_message_received(self, header: str, values: List[str]):
         pass
